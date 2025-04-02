81a28bbf7e77bdde81c9c975a8b9c1c34e13f934
OswaCano <brayan6cn@gmail.com>
2025-04-02 17:13:58 -0600

    form para creacion de post y linkeo con el backend

--- front-react/src/App.jsx
+++ front-react/src/App.jsx
@@ -5,6 +5,7 @@ import Landing from './pages/Landing';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
+import PostForm from './components/posts/PostForm.jsx';
 
 //Paginas de la aplicación
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/posts" element={<PostForm/>} />
       </Routes>
     </Router>
   );
