c5608fbb9b8cf321241e1ae30a67a2a6f9339351
OswaCano <102877514+OswaCano@users.noreply.github.com>
2025-05-04 22:45:03 -0600

    Despliegue en Render de la IA

--- AI-fastapi/main.py
+++ AI-fastapi/main.py
@@ -8,6 +8,10 @@ modelo = joblib.load('modelo_entrenado.pkl')  # o 'modelo_categorias.pkl'
 class EntradaTexto(BaseModel):
     texto: str
 
+@app.get("/")
+def read_root():
+    return {"message": "Aplicación FastAPI desplegada correctamente en Render."}
+
 @app.post("/clasificar")
 def clasificar(data: EntradaTexto):
     pred = modelo.predict([data.texto])
